8c5d563c77dd5db2bba38fc9609dd61ab26312ac
Michael L Perry <michael@qedcode.com>
2020-08-22 15:04:49 -0500

    Extract functions to Crypto class.

--- Java/src/test/java/com/cryptofundamentals/AsymmetricEncryptionTest.java
+++ Java/src/test/java/com/cryptofundamentals/AsymmetricEncryptionTest.java
@@ -5,28 +5,18 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.Security;
-import java.security.Signature;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Test;
 
 public class AsymmetricEncryptionTest {
 
-    static {
-        Security.addProvider(new BouncyCastleProvider());
-    }
-
     @Test
     public void testGenerateRSAKeyPair() throws Exception {
-        var keyPair = generateRsaKey();
+        KeyPair keyPair = Crypto.generateRsaKey();
 
         assertEquals("RSA", keyPair.getPublic().getAlgorithm());
         assertTrue(keyPair.getPublic().getEncoded().length > 2048 / 8);
@@ -35,71 +25,32 @@ public class AsymmetricEncryptionTest {
 
     @Test
     public void testEncryptSymmetricKey() throws Exception {
-        var keyPair = generateRsaKey();
+        KeyPair keyPair = Crypto.generateRsaKey();
 
-        var publicKey = keyPair.getPublic();
-        var privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
 
-        var keyGenerator = KeyGenerator.getInstance("AES", "BC");
-        keyGenerator.init(256);
-        var key = keyGenerator.generateKey();
+        SecretKey key = Crypto.generateAesKey();
 
-        var encryptedKey = encryptWithRsa(publicKey, key);
-        var decryptedKey = decryptWithRsa(privateKey, encryptedKey);
+        byte[] encryptedKey = Crypto.encryptWithRsa(publicKey, key);
+        byte[] decryptedKey = Crypto.decryptWithRsa(privateKey, encryptedKey);
 
         assertArrayEquals(key.getEncoded(), decryptedKey);
     }
 
     @Test
     public void testSignMessage() throws Exception {
-        var keyPair = generateRsaKey();
+        KeyPair keyPair = Crypto.generateRsaKey();
 
-        var publicKey = keyPair.getPublic();
-        var privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
 
-        var message = "Alice knows Bob's secret.";
-        var messageBytes = message.getBytes();
+        String message = "Alice knows Bob's secret.";
+        byte[] messageBytes = message.getBytes();
 
-        var signatureBytes = signMessage(privateKey, messageBytes);
-        var verified = verifySignature(publicKey, messageBytes, signatureBytes);
+        byte[] signatureBytes = Crypto.signMessage(privateKey, messageBytes);
+        boolean verified = Crypto.verifySignature(publicKey, messageBytes, signatureBytes);
 
         assertTrue(verified);
-    }
-
-    private KeyPair generateRsaKey() throws Exception {
-
-        var generator = KeyPairGenerator.getInstance("RSA", "BC");
-        generator.initialize(2048);
-        return generator.generateKeyPair();
-    }
-
-    private byte[] encryptWithRsa(PublicKey publicKey, SecretKey key) throws Exception {
-
-        var rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding", "BC");
-        rsa.init(Cipher.ENCRYPT_MODE, publicKey);
-        return rsa.doFinal(key.getEncoded());
-    }
-
-    private byte[] decryptWithRsa(PrivateKey privateKey, byte[] encryptedKey) throws Exception {
-
-        var rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding", "BC");
-        rsa.init(Cipher.DECRYPT_MODE, privateKey);
-        return rsa.doFinal(encryptedKey);
-    }
-
-    private byte[] signMessage(PrivateKey privateKey, byte[] messageBytes) throws Exception {
-
-        var signature = Signature.getInstance("SHA512withRSA", "BC");
-        signature.initSign(privateKey);
-        signature.update(messageBytes);
-        return signature.sign();
-    }
-
-    private boolean verifySignature(PublicKey publicKey, byte[] messageBytes, byte[] signatureBytes) throws Exception {
-
-        var signature = Signature.getInstance("SHA512withRSA", "BC");
-        signature.initVerify(publicKey);
-        signature.update(messageBytes);
-        return signature.verify(signatureBytes);
     }
 }
--- Java/src/test/java/com/cryptofundamentals/SymmetricEncryptionTest.java
+++ Java/src/test/java/com/cryptofundamentals/SymmetricEncryptionTest.java
@@ -2,35 +2,16 @@ package com.cryptofundamentals;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.security.SecureRandom;
-import java.security.Security;
-
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Test;
 
 public class SymmetricEncryptionTest {
 
-  static {
-    Security.addProvider(new BouncyCastleProvider());
-  }
-
   @Test
   public void testGenerateRandomAESKey() throws Exception {
-    var keyGenerator = KeyGenerator.getInstance("AES", "BC");
-    keyGenerator.init(256);
-    var key = keyGenerator.generateKey();
+    SecretKey key = Crypto.generateAesKey();
 
     assertEquals("AES", key.getAlgorithm());
     assertEquals(32, key.getEncoded().length);
@@ -38,51 +19,14 @@ public class SymmetricEncryptionTest {
 
   @Test
   public void testEncryptAMessageWithAES() throws Exception {
-    var message = "Alice knows Bob's secret.";
+    String inputMessage = "Alice knows Bob's secret.";
 
-    var keyGenerator = KeyGenerator.getInstance("AES", "BC");
-    keyGenerator.init(256);
-    var key = keyGenerator.generateKey();
+    SecretKey key = Crypto.generateAesKey();
+    IvParameterSpec iv = Crypto.generateInitializationVector();
 
-    var random = new SecureRandom();
-    var buffer = new byte[16];
-    random.nextBytes(buffer);
-    var iv = new IvParameterSpec(buffer);
+    byte[] cipertext = Crypto.encryptWithAes(inputMessage, key, iv);
+    String outputMessage = Crypto.decryptWithAes(cipertext, key, iv);
 
-    var cipertext = encryptWithAes(message, key, iv);
-    var actualMessage = decryptWithAes(cipertext, key, iv);
-
-    assertEquals(message, actualMessage);
-  }
-
-  private byte[] encryptWithAes(String message, SecretKey key, IvParameterSpec iv) throws Exception {
-    var out = new ByteArrayOutputStream();
-    var aes = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
-    aes.init(Cipher.ENCRYPT_MODE, key, iv);
-    var cipherOut = new CipherOutputStream(out, aes);
-    var writer = new OutputStreamWriter(cipherOut);
-
-    try {
-      writer.write(message);
-    } finally {
-      writer.close();
-    }
-
-    return out.toByteArray();
-  }
-
-  private String decryptWithAes(byte[] cipertext, SecretKey key, IvParameterSpec iv) throws Exception {
-    var in = new ByteArrayInputStream(cipertext);
-    var aes = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
-    aes.init(Cipher.DECRYPT_MODE, key, iv);
-    var cipherIn = new CipherInputStream(in, aes);
-    var reader = new InputStreamReader(cipherIn);
-    var bufferedReader = new BufferedReader(reader);
-
-    try {
-      return bufferedReader.readLine();
-    } finally {
-      bufferedReader.close();
-    }
+    assertEquals(inputMessage, outputMessage);
   }
 }
