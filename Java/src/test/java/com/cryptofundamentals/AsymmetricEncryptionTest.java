83540dbe38205f2892676326a2d97dca1ad669b7
Michael L Perry <michael@qedcode.com>
2020-08-22 13:49:08 -0500

    Use Bouncy Castle for asymmetric encryption too.

--- Java/src/test/java/com/cryptofundamentals/AsymmetricEncryptionTest.java
+++ Java/src/test/java/com/cryptofundamentals/AsymmetricEncryptionTest.java
@@ -6,18 +6,23 @@ import static org.junit.Assert.assertTrue;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Security;
 import java.security.Signature;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Test;
 
 public class AsymmetricEncryptionTest {
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     @Test
     public void testGenerateRSAKeyPair() throws Exception {
@@ -35,7 +40,7 @@ public class AsymmetricEncryptionTest {
         var publicKey = keyPair.getPublic();
         var privateKey = keyPair.getPrivate();
 
-        var keyGenerator = KeyGenerator.getInstance("AES");
+        var keyGenerator = KeyGenerator.getInstance("AES", "BC");
         keyGenerator.init(256);
         var key = keyGenerator.generateKey();
 
@@ -61,29 +66,30 @@ public class AsymmetricEncryptionTest {
         assertTrue(verified);
     }
 
-    private KeyPair generateRsaKey() throws NoSuchAlgorithmException {
-        var generator = KeyPairGenerator.getInstance("RSA");
+    private KeyPair generateRsaKey() throws Exception {
+
+        var generator = KeyPairGenerator.getInstance("RSA", "BC");
         generator.initialize(2048);
         return generator.generateKeyPair();
     }
 
     private byte[] encryptWithRsa(PublicKey publicKey, SecretKey key) throws Exception {
 
-        var rsa = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+        var rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding", "BC");
         rsa.init(Cipher.ENCRYPT_MODE, publicKey);
         return rsa.doFinal(key.getEncoded());
     }
 
     private byte[] decryptWithRsa(PrivateKey privateKey, byte[] encryptedKey) throws Exception {
 
-        var rsa = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+        var rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding", "BC");
         rsa.init(Cipher.DECRYPT_MODE, privateKey);
         return rsa.doFinal(encryptedKey);
     }
 
     private byte[] signMessage(PrivateKey privateKey, byte[] messageBytes) throws Exception {
 
-        var signature = Signature.getInstance("SHA256withRSA");
+        var signature = Signature.getInstance("SHA512withRSA", "BC");
         signature.initSign(privateKey);
         signature.update(messageBytes);
         return signature.sign();
@@ -91,7 +97,7 @@ public class AsymmetricEncryptionTest {
 
     private boolean verifySignature(PublicKey publicKey, byte[] messageBytes, byte[] signatureBytes) throws Exception {
 
-        var signature = Signature.getInstance("SHA256withRSA");
+        var signature = Signature.getInstance("SHA512withRSA", "BC");
         signature.initVerify(publicKey);
         signature.update(messageBytes);
         return signature.verify(signatureBytes);
