86975fc33dcecaf3b4ff9656e1606d9c8cbd3056
Michael L Perry <michael@qedcode.com>
2020-08-22 17:02:20 -0500

    Added certificate helper.

---
+++ Java/src/main/java/com/cryptofundamentals/CertificateHelpers.java
@@ -0,0 +1,21 @@
+package com.cryptofundamentals;
+
+import java.io.StringReader;
+
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.openssl.PEMParser;
+
+public class CertificateHelpers {
+    
+    static {
+        UseBouncyCastle.please();
+    }
+
+	public static X509CertificateHolder loadCertificate(String pem) throws Exception {
+        var reader = new StringReader(pem);
+        var parser = new PEMParser(reader);
+        var certificateHolder = (X509CertificateHolder)parser.readObject();
+		return certificateHolder;
+	}
+
+}
