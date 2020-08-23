1498b71263595ed90cdbcbe740876adf41b57fee
Michael L Perry <michael@qedcode.com>
2020-08-22 23:20:16 -0500

    Added asymmetric encryption tests.

---
+++ .NET/CryptoHelpers/CryptoHelpers.Test/AsymmetricEncryptionTest.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace CryptoHelpers.Test
+{
+    public class AsymmetricEncryptionTest
+    {
+        [Fact]
+        public void GenerateRsaKeyPair()
+        {
+            var rsa = RSA.Create(2048);
+
+            rsa.KeySize.Should().Be(2048);
+        }
+
+        [Fact]
+        public void SharePublicKey()
+        {
+            var rsa = RSA.Create(2048);
+            var publicKey = rsa.ExportRSAPublicKey();
+            publicKey.Length.Should().Be((2048 + 112) / 8);
+        }
+
+        [Fact]
+        public void EncryptSymmetricKey()
+        {
+            var rsa = RSA.Create(2048);
+            var publicKey = rsa.ExportRSAPublicKey();
+
+            var key = SymmetricAlgorithm.Create("AES").Key;
+
+            byte[] encryptedKey = AsymmetricHelpers.EncryptWithRsa(publicKey, key);
+            byte[] decryptedKey = AsymmetricHelpers.DecryptWithRsa(rsa, encryptedKey);
+
+            decryptedKey.Should().Equal(key);
+        }
+
+        [Fact]
+        public void SignMessage()
+        {
+            var rsa = RSA.Create(2048);
+            var publicKey = rsa.ExportRSAPublicKey();
+
+            string message = "Alice knows Bob's secret.";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            byte[] signatureBytes = AsymmetricHelpers.SignMessage(rsa, messageBytes);
+            bool verified = AsymmetricHelpers.VerifySignature(publicKey, messageBytes, signatureBytes);
+
+            verified.Should().BeTrue();
+        }
+    }
+}
---
+++ .NET/CryptoHelpers/CryptoHelpers/AsymmetricHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CryptoHelpers
+{
+    public class AsymmetricHelpers
+    {
+        public static byte[] EncryptWithRsa(byte[] publicKey, byte[] plaintext)
+        {
+            var parameters = CreateParameters(publicKey);
+            var rsa = RSA.Create(parameters);
+            var ciphertext = rsa.Encrypt(plaintext, RSAEncryptionPadding.OaepSHA512);
+
+            return ciphertext;
+        }
+
+        public static byte[] DecryptWithRsa(RSA rsa, byte[] ciphertext)
+        {
+            var plaintext = rsa.Decrypt(ciphertext, RSAEncryptionPadding.OaepSHA512);
+            return plaintext;
+        }
+
+        public static byte[] SignMessage(RSA rsa, byte[] messageBytes)
+        {
+            return rsa.SignData(messageBytes, HashAlgorithmName.SHA512, RSASignaturePadding.Pkcs1);
+        }
+
+        public static bool VerifySignature(byte[] publicKey, byte[] messageBytes, byte[] signatureBytes)
+        {
+            var parameters = CreateParameters(publicKey);
+            var rsa = RSA.Create(parameters);
+            return rsa.VerifyData(messageBytes, signatureBytes, HashAlgorithmName.SHA512, RSASignaturePadding.Pkcs1);
+        }
+
+        private static RSAParameters CreateParameters(byte[] publicKey)
+        {
+            var modulus = new byte[2048 / 8];
+            Array.Copy(publicKey, 9, modulus, 0, 2048 / 8);
+            var exponent = new byte[3];
+            Array.Copy(publicKey, 11 + 2048 / 8, exponent, 0, 3);
+
+            return new RSAParameters
+            {
+                Exponent = exponent,
+                Modulus = modulus
+            };
+        }
+    }
+}
